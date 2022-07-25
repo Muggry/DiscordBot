d95c9abe91564558ed9cd64762f87dd3c6f0bee2
Muggry <chrisoldg@icloud.com>
2022-07-24 21:27:53 -0400

    added gitinore

--- Main/main.js
+++ Main/main.js
@@ -10,23 +10,47 @@ var error = function(s) {
     console.error(s)
 }
 
-var prefix = ".";
 
+const Discord = require('discord.js');
 
-const discord = require('discord.js');
-
-const client = new discord.Client({
+const client = new Discord.Client({
     allowedMentions: {
-        parse: ['users', 'roles'],
+        parse: [
+            'users',
+            'roles',
+        ],
         repliedUser: true,
     },
     intents: [
-        "GUILDS",
-        "GUILD_MESSAGES",
-        "GUILD_PRESENCES",
-        "GUILD_MESSAGE_REACTIONS",
+        Discord.Intents.FLAGS.GUILDS,
+        Discord.Intents.FLAGS.GUILD_MEMBERS,
+        Discord.Intents.FLAGS.GUILD_MESSAGES,
+        Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+        Discord.Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+        Discord.Intents.FLAGS.GUILD_INTEGRATIONS,
+        Discord.Intents.FLAGS.GUILD_WEBHOOKS,
+        Discord.Intents.FLAGS.GUILD_INVITES,
+        Discord.Intents.FLAGS.GUILD_BANS,
+        Discord.Intents.FLAGS.DIRECT_MESSAGES,
     ],
-})
+    partials: [
+        'CHANNEL',
+    ],
+    presence: {
+        status: 'online',
+        activities: [
+            {
+                type: 'LISTENING',
+                name: `${process.env.BOT_COMMAND_PREFIX}help`,
+            },
+        ],
+    },
+});
+
+client.$ = {
+    commands: new Discord.Collection(),
+    interactions: new Discord.Collection(),
+};
 
 
 
@@ -36,7 +60,7 @@ client.once('ready', () => {
     print("Bot is ready")
 });
 
-client.on("messageCreate", message => {
+client.on("messageCreate", (message) => {
     if (!message.content.startsWith(prefix) || message.author.bot) return;
 
     const args = message.content.slice(prefix.length).split(/ +/)
@@ -47,5 +71,5 @@ client.on("messageCreate", message => {
 
 
 
-const token = "MTAwMDg1OTAxNjEwMjE1MDI3NA.GO_32w.nEjlNjRxFqJYIlcIcaaoMRU63Z1ixoX5UR2mOg";
+const token = process.env.BOT_TOKEN;
 client.login(token)
